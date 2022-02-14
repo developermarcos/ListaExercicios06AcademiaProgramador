daa80d00e72cfb3ee43268a8f5f9e87d2d5f2429
Marcos Lima <developermarcos@outlook.com>
2022-02-14 14:51:41 -0300

    Exercicio 1 finalizado

--- Exercicio01.ConsoleApp/Program.cs
+++ Exercicio01.ConsoleApp/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+//-Desenhe um retângulo com asteriscos, considere que o primeiro input é a largura do retângulo, e o segundo input é a altura.
 namespace Exercicio01.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int larguraRetangulo = 50, alturaRetangulo = 10;
+            string imprimirTela = "";
+            for (int i = 0; i < alturaRetangulo; i++)
+            {
+                for (int z = 0; z < larguraRetangulo; z++)
+                {
+                    imprimirTela +="*";
+                }
+                imprimirTela +="\n";
+            }
+            Console.WriteLine(imprimirTela);
+            Console.ReadKey();
         }
     }
 }
