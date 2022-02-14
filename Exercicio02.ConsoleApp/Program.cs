2affb7e9d796318ea5158ede16dbfa357b55e656
Marcos Lima <developermarcos@outlook.com>
2022-02-14 14:55:32 -0300

    Exercicio 2 finalizado

--- Exercicio02.ConsoleApp/Program.cs
+++ Exercicio02.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//Desenhe um retângulo com A e B, onde as forem linhas pares escreva "A" e as linhas ímpares escreva "B";
 
 namespace Exercicio02.ConsoleApp
 {
@@ -6,7 +7,26 @@ namespace Exercicio02.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int larguraRetangulo = 50, alturaRetangulo = 10;
+            string imprimirTela = "";
+            for (int i = 0; i < alturaRetangulo; i++)
+            {
+                for (int z = 0; z < larguraRetangulo; z++)
+                {
+                    if(i %2 == 0)
+                    {
+                        imprimirTela +="A";
+                    }
+                    else
+                    {
+                        imprimirTela +="B";
+                    }
+                    
+                }
+                imprimirTela +="\n";
+            }
+            Console.WriteLine(imprimirTela);
+            Console.ReadKey();
         }
     }
 }
